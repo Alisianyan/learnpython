92a98444d60d341990beb50bf0e9881c5d7861ee
Elisia <elisialight@gmail.com>
2021-01-14 21:24:24 +0300

    webinar messenger

---
+++ find_min_func.py
@@ -0,0 +1,6 @@
+def find_min(a*):
+    minium=a[0]
+    for i in a:
+        if minium > x:
+            minium = x
+    return minium
---
+++ skillbox/skillbox-webinar.py
@@ -0,0 +1,27 @@
+name=input('message')
+print(f'Hello, {name}')
+
+def echo_message(text, time, neme):
+	message['text'], message['time'], message['name]
+
+
+	
+message = {
+	'text':'text',
+	'time':'timestamp',
+	'name':'Nick'
+}
+
+db = [
+	{
+	'text':'1',
+	'time':'timestamp',
+	'name':'masha'
+	},
+	{'text':'2'}
+]
+
+def print_messages(db):
+	for message in db:
+			echo_message(message)
+	
